6f0a0c9d422c722d0288cafce2d2db6fd362d91a
Tanuj Mishra <thetanuj1@gmail.com>
2018-12-22 20:30:23 +0530

    Change in package name

--- android/app/src/main/java/com/professionalbakait/calculator/MainActivity.java
+++ android/app/src/main/java/com/professionalbakait/calculator/MainActivity.java
@@ -1,4 +1,4 @@
-package com.calculator;
+package com.professionalbakait.calculator;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/professionalbakait/calculator/MainApplication.java
+++ android/app/src/main/java/com/professionalbakait/calculator/MainApplication.java
@@ -1,4 +1,4 @@
-package com.calculator;
+package com.professionalbakait.calculator;
 
 import android.app.Application;
 
